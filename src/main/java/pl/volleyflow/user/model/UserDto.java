48fa9b8f92ddd3497605c121e3dcf46723e2f6b1
mati <kaczmarskim95@gmail.com>
2026-01-07 21:25:51 +0100

    Add user dto

---
+++ src/main/java/pl/volleyflow/user/model/UserDto.java
@@ -0,0 +1,15 @@
+package pl.volleyflow.user.model;
+
+import lombok.Builder;
+
+import java.util.UUID;
+
+@Builder
+public record UserDto(
+        UUID externalId,
+        String firstName,
+        String lastName,
+        String email,
+        String avatar
+) {
+}
