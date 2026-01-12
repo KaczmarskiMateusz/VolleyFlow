a8f7b750b9d416bb683a51a66051c78c045de8ce
mati <kaczmarskim95@gmail.com>
2026-01-12 22:22:39 +0100

    Add auth controller

---
+++ src/main/java/pl/volleyflow/authorization/controller/AuthController.java
@@ -0,0 +1,36 @@
+package pl.volleyflow.authorization.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.volleyflow.authorization.model.AuthResponse;
+import pl.volleyflow.authorization.model.UserLoginRequest;
+import pl.volleyflow.authorization.service.AuthService;
+import pl.volleyflow.authorization.model.UserRegisterRequest;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/app/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> registerUser(@Valid @RequestBody UserRegisterRequest request) {
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody UserLoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
