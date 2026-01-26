6f5743e30e0379c23cbce156212871ed195a1213
mati <kaczmarskim95@gmail.com>
2026-01-26 21:31:13 +0100

    Add EmailAlreadyExistsException

---
+++ src/main/java/pl/volleyflow/user/controller/UserExceptionHandler.java
@@ -0,0 +1,23 @@
+package pl.volleyflow.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.volleyflow.authorization.model.ApiErrorResponse;
+import pl.volleyflow.user.service.EmailAlreadyExistsException;
+import pl.volleyflow.user.service.InvalidCredentialsException;
+
+import static pl.volleyflow.authorization.model.ErrorCode.AUTH_INVALID_CREDENTIALS;
+import static pl.volleyflow.authorization.model.ErrorCode.EMAIL_ALREADY_EXISTS;
+
+@RestControllerAdvice
+public class UserExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ApiErrorResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        ApiErrorResponse body = new ApiErrorResponse(EMAIL_ALREADY_EXISTS, "Email already exists");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(body);
+    }
+
+}
