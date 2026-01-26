bd934e7d9e51e8c29361b4c394475ec72a19abd9
mati <kaczmarskim95@gmail.com>
2026-01-26 11:01:36 +0100

    Add UserAccountRepository

---
+++ src/main/java/pl/volleyflow/user/repository/UserAccountRepository.java
@@ -0,0 +1,16 @@
+package pl.volleyflow.user.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.volleyflow.user.model.UserAccount;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
+
+    Optional<UserAccount> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Optional<UserAccount> findByExternalId(UUID externalId);
+}
