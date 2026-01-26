3db465b58ddef63099fbf38c10f25b2f7bbe19de
mati <kaczmarskim95@gmail.com>
2026-01-26 11:01:07 +0100

    Add UserLoginRequest

--- src/main/java/pl/volleyflow/authorization/model/UserLoginRequest.java
+++ src/main/java/pl/volleyflow/authorization/model/UserLoginRequest.java
@@ -12,5 +12,4 @@ public record UserLoginRequest(
         @NotBlank(message = "Password is required")
         @Size(min = 8, max = 72, message = "Password must be between 8 and 72 characters")
         String password
-
 ) {}
