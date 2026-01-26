eb8efc9916bebc5acf67402021fe86476ffe2c4a
mati <kaczmarskim95@gmail.com>
2026-01-26 10:39:19 +0100

    Add AdviceClubControllerHandler

---
+++ src/main/java/pl/volleyflow/authorization/controller/AuthExceptionHandler.java
@@ -0,0 +1,21 @@
+package pl.volleyflow.authorization.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.volleyflow.authorization.model.ApiErrorResponse;
+import pl.volleyflow.user.service.InvalidCredentialsException;
+
+import static pl.volleyflow.authorization.model.ErrorCode.AUTH_INVALID_CREDENTIALS;
+
+@RestControllerAdvice
+public class AuthExceptionHandler {
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ApiErrorResponse> handleInvalidCredentials(InvalidCredentialsException ex) {
+        ApiErrorResponse body = new ApiErrorResponse(AUTH_INVALID_CREDENTIALS, "Invalid username or password");
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
+    }
+
+}
