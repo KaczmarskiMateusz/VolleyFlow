020964245af3edb2132b99d9b25454036b059d8a
mati <kaczmarskim95@gmail.com>
2026-01-26 10:39:49 +0100

    Add ClubDto

---
+++ src/main/java/pl/volleyflow/club/model/ClubDto.java
@@ -0,0 +1,20 @@
+package pl.volleyflow.club.model;
+
+import lombok.Builder;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Builder
+public record ClubDto(
+        UUID externalId,
+        String name,
+        String description,
+        String city,
+        String logoUrl,
+        ClubStatus status,
+        Instant createdAt,
+        Instant updatedAt,
+        Integer version
+) {
+}
