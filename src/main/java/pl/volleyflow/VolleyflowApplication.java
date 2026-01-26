9bc8f6972cbb463713c4181c1e58cb0d8f447ea3
mati <kaczmarskim95@gmail.com>
2026-01-26 21:13:18 +0100

    config

--- src/main/java/pl/volleyflow/VolleyflowApplication.java
+++ src/main/java/pl/volleyflow/VolleyflowApplication.java
@@ -2,8 +2,10 @@ package pl.volleyflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class VolleyflowApplication {
 
 	public static void main(String[] args) {
