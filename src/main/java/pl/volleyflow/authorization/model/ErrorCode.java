7d1a448ea100cc7a75980a9005fb7cbd9b71eec1
mati <kaczmarskim95@gmail.com>
2026-02-07 12:16:20 +0100

    Add error codes

--- src/main/java/pl/volleyflow/authorization/model/ErrorCode.java
+++ src/main/java/pl/volleyflow/authorization/model/ErrorCode.java
@@ -1,6 +1,19 @@
 package pl.volleyflow.authorization.model;
 
 public enum ErrorCode {
+
+    //AUTH
     AUTH_INVALID_CREDENTIALS,
-    EMAIL_ALREADY_EXISTS
+    EMAIL_ALREADY_EXISTS,
+
+
+    //USER
+
+
+    // CLUB
+    CLUB_PERMISSION_DENIED,
+
+
+    // CLUB MEMBER
+
 }
