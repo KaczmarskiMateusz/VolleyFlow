504ef83563941b5e1f78a2e81df13b96cb7fe667
mati <kaczmarskim95@gmail.com>
2026-03-05 20:58:53 +0100

    feat: extend member profile

---
+++ src/main/java/pl/volleyflow/clubmember/model/DominantHand.java
@@ -0,0 +1,8 @@
+package pl.volleyflow.clubmember.model;
+
+public enum DominantHand {
+    UNKNOWN,
+    RIGHT,
+    LEFT,
+    AMBIDEXTROUS
+}
---
+++ src/main/java/pl/volleyflow/clubmember/model/Gender.java
@@ -0,0 +1,8 @@
+package pl.volleyflow.clubmember.model;
+
+public enum Gender {
+    UNSPECIFIED,
+    FEMALE,
+    MALE,
+    OTHER
+}
